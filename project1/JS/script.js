4a804d660ca75858edbe739864251ffe3932b17c
Leonidgit <leonidu36@gmail.com>
2022-11-08 17:03:27 +0300

    muvi2

--- project1/JS/script.js
+++ project1/JS/script.js
@@ -7,12 +7,33 @@ const personalMovieDB = {
     privat: false,
 };
 
-const a = prompt('Один из последних просмотренныйх фильмов?', ''),
-      b = +prompt('На сколько вы его оцените?', ''),
-      c = prompt('Один из последних просмотренныйх фильмов?', ''),
-      d = +prompt('На сколько вы его оцените?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for(let i = 0; i < 2; i++){
+    const a = prompt('Один из последних просмотренныйх фильмов?', ''),
+      b = +prompt('На сколько вы его оцените?', '');
+    
+      if(a != null && b != null && a != '' && a.length < 50){
+        personalMovieDB.movies[a] = b;
+        console.log(personalMovieDB);
 
-console.log(personalMovieDB);
+      }else{
+        i--;
+      }
+      
+
+
+}
+if(personalMovieDB.count < 10){
+    console.log('Посмотренно довольно мало фильмов');
+}else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    console.log('Вы классический зритель');
+}else if(personalMovieDB.count >=30){
+    console.log('Вы киноман');
+}else {
+    console.log('Ошибка');
+}
+
+
+
+
+
